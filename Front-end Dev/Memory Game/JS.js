65873585009cbb2cac6cb375d058e637664c3fb1
AdamVish <adamix.ws@gmail.com>
2023-11-08 13:06:04 +0100

    More front end

---
+++ Front-end Dev/Memory Game/JS.js
@@ -0,0 +1,9 @@
+function onclick(){
+    var clicked = document.getElementsById("card")
+    if(clicked.style.color =="pink"){
+        clicked.style.color == "black";
+    }
+    else {
+        clicked.style.color =="pink";
+    }
+}
